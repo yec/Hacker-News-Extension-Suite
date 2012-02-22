d2580999e5e223c8e656ecce38a017f468333f7b
Ye Chuah <yechuah@gmail.com>
2012-02-22 23:34:08 +1100

    Update hackernews.js

--- hackernews.js
+++ hackernews.js
@@ -1,10 +1,12 @@
 var imgs = document.getElementsByTagName('img');
 var comments = [];
 var commentcount = 0;
+var doCollapse;
 
-function handleComment(commentindex) {
+function handleComment(commentindex, isRoot) {
     var comment = comments[commentindex]['node'].getElementsByTagName('span');
-	
+	var p = comments[commentindex]['node'].getElementsByTagName('p')[0];
+
 	for (i in comment) {
 		if (comment[i].getAttribute('class') == 'comment') {
 			comment = comment[i];
@@ -12,13 +14,31 @@ function handleComment(commentindex) {
 		}
 	}
     
-	if (comment.style.display == 'none') {
-        comment.style.display = 'block';
-    } else {
-        comment.style.display = 'none';
-    }
+	if (isRoot) {
+		if (comment.style.display == 'none') {
+			doCollapse = false;
+	        comment.style.display = 'block';
+		    p.style.display = 'block';
+	    } else {
+			doCollapse = true;
+	        comment.style.display = 'none';
+			p.style.display = 'none';
+	    }
+	} else {
+		if (doCollapse) {
+	        comments[commentindex]['node'].style.display = 'none';
+			comment.style.display = 'none';
+			p.style.display = 'none';
+	    } else {
+	        comments[commentindex]['node'].style.display = 'block';
+			comment.style.display = 'block';
+			p.style.display = 'block';
+	    }
+	}
+
+
     for (var i in comments[commentindex]['children']) {
-        handleComment(comments[commentindex]['children'][i]);
+        handleComment(comments[commentindex]['children'][i], false);
     }
 }
 
@@ -38,17 +58,16 @@ for (i in imgs) {
         var width = imgs[i].getAttribute('width');
         var depth = (parseInt(width)/40);
         if (depth % 1 == 0 ) {
-            title.innerHTML += '<br />' + (parseInt(width)/40);
             comments[commentcount] = {};
             comments[commentcount]['node'] = imgs[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-            imgs[i].parentNode.parentNode.childNodes[2].childNodes[0].innerHTML += ' <a href="javascript:handleComment('+commentcount+')">(toggle comments)</a>';
+            imgs[i].parentNode.parentNode.childNodes[2].childNodes[0].innerHTML += ' <a href="javascript:handleComment('+commentcount+', true)">(toggle comments)</a>';
             /*comments[commentcount]['node'].style.display = 'none';*/
             comments[commentcount]['depth'] = depth;
             comments[commentcount]['children'] = [];
             
 			parent[depth] = comments[commentcount];
 
-			if (commentcount > 0) {
+			if (depth > 0) {
             	parent[depth - 1]['children'].push(commentcount);
             }
             commentcount +=1;
